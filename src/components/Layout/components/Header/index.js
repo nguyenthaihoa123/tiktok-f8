8353ccd273841fafe32cd301bbe52017f5b2c66c
Hoa <52100413@student.tdtu.edu.vn>
2023-09-08 00:03:20 +0700

    8/9

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,31 @@
-import styles from './Header.module.scss'
+import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
-
-const cx = classNames.bind(styles)
+import images from '~/assets/images/index'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner,faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'; 
+const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            
-        </div>
-    </header>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt='logo'/>
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder='Search accounts and videos' spellCheck={false}/>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark}/>
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
